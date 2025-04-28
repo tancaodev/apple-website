1d406ec7cd17542950cbc98c5ed2546ecf377502
Tan Cao <tancaodev.github@gmail.com>
2025-04-28 20:47:15 +0700

    update: Change website tab title

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-// import * as Sentry from "@sentry/react";
-
-// Sentry.init({
-//   dsn: "https://67b337deda64fa382cd32991c3123bd0@o4508969281388544.ingest.us.sentry.io/4508969287483392",
-//   integrations: [
-//     Sentry.reactRouterV6BrowserTracingIntegration({
-//       useEffect: React.useEffect
-//     }),
-//   ]
-// });
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
